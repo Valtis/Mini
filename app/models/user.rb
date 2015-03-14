c27cea93961e06778e71814a2fa694b9ac885616
Valtis <erkka.kaaria@helsinki.fi>
2015-03-14 14:46:23 +0200

    User scaffolding. User password digest. User validations

---
+++ app/models/user.rb
@@ -0,0 +1,61 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+
+  validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :password, length: { minimum: 8 }
+  validate :password_contains_capital_letter_and_number
+  validate :password_does_not_contain_username
+  validate :status_has_valid_range
+
+  def password_contains_capital_letter_and_number
+    if not password =~ /[A-Z]/ and not password =~ /[0-9]/
+      errors.add(:password, 'must contain at least one capital letter and a number')
+    end
+  end
+
+  def password_does_not_contain_username
+    if password.downcase.include? username.downcase
+      errors.add(:password, 'must not contain username')
+    end
+  end
+
+  def status_has_valid_range
+    if status < 0 or status > 3
+      error(:status, 'has invalid value')
+    end
+  end
+
+
+  def set_status(status)
+    case status
+      when :normal
+        self.status = 0
+      when :banned
+        self.status = 1
+      when :moderator
+        self.status = 2
+      when :admin
+        self.status = 3
+      else
+        throw 'Invalid user status code'
+    end
+  end
+
+  def user_status
+    case self.status
+      when 0
+        return :normal
+      when 1
+        return :banned
+      when 2
+        return :moderator
+      when 3
+        return :admin
+      else
+        throw 'Invalid user status, something went horribly wrong'
+    end
+  end
+
+
+end
