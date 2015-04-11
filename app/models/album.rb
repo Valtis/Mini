38e3af7416dcdff1f8f49e198bdde74951ffc2f9
Valtis <erkka.kaaria@helsinki.fi>
2015-04-11 13:37:57 +0300

    Added validations to album, few tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,8 @@ class Album < ActiveRecord::Base
 
   has_many :images
   belongs_to :user
+
+  validates :name, presence: true
+  validates :user, presence: true
+
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is created successfully with name and user' do
+    user = FactoryGirl.create :user
+    album = Album.create user: user, name: 'hello'
+
+    expect(Album.count).to eq(1)
+    expect(album).to be_valid
+    expect(album.user).to be(user)
+    expect(album.name).to eq('hello')
+  end
+
+  it 'cannot be created without user' do
+     album = Album.create name: 'hello'
+
+    expect(Album.count).to eq(0)
+    expect(album).not_to be_valid
+  end
+
+  it 'cannot be created without name' do
+    user = FactoryGirl.create :user
+    album = Album.create user: user
+
+    expect(Album.count).to eq(0)
+    expect(album).not_to be_valid
+  end
+
+
 end
